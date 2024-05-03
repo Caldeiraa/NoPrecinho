632a49dd8d9be313874e06ceed8639dc2d6d13ca
Pedro Henrique Moraes Merlo <115297797+PedroHmerlo@users.noreply.github.com>
2024-05-03 20:56:14 -0300

    Lista de usuários

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import Home from './views/Home/index'
+import ListaUsu from './views/ListaUsuarios/ListaUsuarios'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/usuarios' element={<ListaUsu/>}/>
       </Routes>
     </BrowserRouter>
   );
