1fd453de9217b1f2553fb855a798d43b66e83dae
pedrohenriquedeveloper <115297797+pedrohenriquedeveloper@users.noreply.github.com>
2024-05-06 17:01:00 -0300

    atualizações

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,95 +1,77 @@
-import React from 'react'
-import np from './img/NP.png'
-import mc from './img/mercearia.png'
-import cn from './img/carne.png'
-import bd from './img/bebidas.png'
-import lp from './img/limpeza.png'
-import pc from './img/pereciveis.png'
-import pd from './img/padaria.png'
-import ht from './img/hortifruit.png'
-import hg from './img/higiêne.png'
+import React from 'react';
 
-function index() {
+function Home() {
   return (
-        <div class="conteudo">
-        <div class="container-fluid p-0 text-center d-flex flex-column">
-            <div class="container-fluid p-0 text-center cabecalho">
-                <nav class="navbar navbar-expand-lg d-flex justify-content-center align-items-center">
-                    <a class="navbar-brand fs-4" href="#">NoPrecinho</a>
-                    <img class="logo" src={np} alt=""/>
-                </nav>
-                
+    <div className="conteudo">
+      <div className="cabecalho">
+        <nav className="navbar navbar-expand-lg fixed-top">
+          <div className="container-fluid d-flex justify-content-around">
+            <a className="navbar-brand me-auto" href="#">NoPrecinho</a>
+            <img className="logo me-auto" src="img/NP.png" alt="NoPrecinho Logo" />
+            <div className="offcanvas offcanvas-end oi" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel" style={{ color: 'white' }}>jose</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav flex-grow-1 ms-5 justify-content-center">
+                  <li className="nav-item">
+                    <a className="nav-link mx-lg-2" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link mx-lg-2" href="#">Sobre</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link mx-lg-2" href="#">Serviços</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link mx-lg-2" href="#">Portfólios</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link mx-lg-2 mr-5" href="#">Contato</a>
+                  </li>
+                  <li className="ms-5">
+                    <form className="d-flex" role="search">
+                      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                      <button className="btn " type="submit">Search</button>
+                    </form>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <h1 class="text-center mt-2">Olá, o que você quer anunciar?</h1>
+            <a href="#" className="login-button">Login</a>
+            <button className="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </nav>
+      </div>
 
-            <div class="container-fluid d-flex justify-content-evenly align-items-center">
-                <div class="col-box d-flex flex-column align-items-center justify-content-evenly">
-                    <div class="box-c" id="1">
-                        <a href="">
-                        <img  class="ms-4" src={mc} alt=""/>
-                            <h3>Mercearia</h3> 
-                        </a>
-                    </div>
-
-                    <div class="box-c" id="2">
-                        <a href="">
-                            <img src={pc} alt=""/>
-                            <h3>Pereciveis</h3> 
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-box d-flex flex-column align-items-center justify-content-evenly">
-                    <div class="box-c" id="3    ">
-                        <a href="">
-                            <img src={cn} alt=""/>
-                            <h3>Carne</h3> 
-                        </a>
-                    </div>
-
-                    <div class="box-c" id="4">
-                        <a href="">
-                            <img src={pd    } alt=""/>
-                            <h3>Padaria</h3> 
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-box d-flex flex-column align-items-center justify-content-evenly">
-                    <div class="box-c" id="5">
-                        <a href="">
-                            <img src={bd} alt=""/>
-                            <h3>Bebidas</h3> 
-                        </a>
-                    </div>
-
-                    <div class="box-c" id="6">
-                        <a href="">
-                            <img src={ht} alt=""/>
-                            <h3>Hortifruit</h3> 
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-box d-flex flex-column align-items-center justify-content-evenly">
-                    <div class="box-c" id="7">
-                        <a href="">
-                            <img src={lp} alt=""/>
-                            <h3>Limpeza</h3> 
-                        </a>
-                    </div>
-                    
-                    <div class="box-c" id="8">
-                        <a href="">
-                            <img src={hg} alt=""/>
-                            <h3>Higiene</h3> 
-                        </a>
-                    </div>
-                </div>
-            </div>
+      <div className="container parceiros">
+        <h3>Parceiros</h3>
+        <div className="slider-wrapper">
+          <ul className="image-list">
+            <li><img className="image-item" src="img/Carrefour-Logo.png" alt="Carrefour Logo" /></li>
+            <li><img className="image-item tt" src="img/extrabom-2x.webp" alt="Extrabom Logo" /></li>
+            <li><img className="image-item" src="img/header-logo.png" alt="Header Logo" /></li>
+            <li><img className="image-item" src="img/logo-bh-white.png" alt="BH Logo" /></li>
+            <li><img className="image-item" src="img/Logo_assai.png" alt="Assai Logo" /></li>
+            <li><img className="image-item tt" src="img/extrabom-2x.webp" alt="Extrabom Logo" /></li>
+            <li><img className="image-item" src="img/Carrefour-Logo.png" alt="Carrefour Logo" /></li>
+            <li><img className="image-item" src="img/logo-bh-white.png" alt="BH Logo" /></li>
+            <li><img className="image-item" src="img/header-logo.png" alt="Header Logo" /></li>
+            <li><img className="image-item" src="img/Logo_assai.png" alt="Assai Logo" /></li>
+          </ul>
         </div>
+        <div className="slider-scrollbar">
+          <div className="scrollbar-track">
+            <div className="scrollbar-thumb"></div>
+          </div>
+        </div>
+        <h3>Promoções</h3>
+      </div>
     </div>
-    )
+  );
 }
 
-export default index
+export default Home;
