929137cbfb44a2dc7455dbd91c16df71027826b0
Pedro Henrique Moraes Merlo <115297797+PedroHmerlo@users.noreply.github.com>
2024-05-06 21:43:47 -0300

    att

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import categorias from './views/Home/categorias'
+import Categorias from './views/categorias/categorias'
 import InserirUsuario from './views/InserirUsuario/inserirUsuario';
 import InserirMercado from './views/InserirMercado/inserirMercado';
 import ListaUsu from './views/ListaUsuarios/ListaUsuarios'
+import Home from './views/Home/index'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/categoria' element={<categorias/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/categoria' element={<Categorias/>}/>
         <Route path='/cadastro/usuario' element={<InserirUsuario/>}/>
         <Route path='/cadastro/mercado' element={<InserirMercado/>}/>
         <Route path='/usuarios' element={<ListaUsu/>}/>
