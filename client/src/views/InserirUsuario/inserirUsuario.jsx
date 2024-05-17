55e7022fefed0a84dda648ef688397329c776688
pedrohenriquedeveloper <115297797+pedrohenriquedeveloper@users.noreply.github.com>
2024-05-17 15:02:01 -0300

    atualizações nas telas de cadastro do produtos

--- client/src/views/InserirUsuario/inserirUsuario.jsx
+++ client/src/views/InserirUsuario/inserirUsuario.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './estilo.css'
 
 function InserirUsuario() {
--- client/src/views/cadastroProduto/cadastroProdMer.jsx
+++ client/src/views/cadastroProduto/cadastroProdMer.jsx
@@ -1,28 +1,68 @@
-import React from 'react'
+import { useState } from "react";
 
- function cadastroProdMer() {
+
+function CadastroProdMer() {
+    const[nome_produto, setNome_produto] = useState('')
+    const[marca_produto, setMarca_produto] = useState('')
+    const[peso_produto, setPeso_produto] = useState('')
+    const[preco_produto, setPreco_produto] = useState('')
+    const[foto_produto, setFoto_produto] = useState('')
+    const[descricao_produto, setDescricao_produto] = useState('')
+
+    async function cadastrarProdutos(event){
+        event.preventDefault()
+        const produtoData = {
+            nome_produto,
+            marca_produto,
+            peso_produto,
+            preco_produto,
+            foto_produto,
+            descricao_produto
+        }
+        try {
+            const resposta = await fetch('/cadastroPMer',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(produtoData)
+            })
+            if(!resposta.ok){
+                throw resposta
+                console.debug("Erro  ao cadastrar produto")
+            }else{
+                console.debug("Produto cadastrado")
+                alert("Cadastrado")
+            }
+        } catch (error) {
+            console.debug(error)
+        }
+    }
+
+
     return (
         <div class="conteudo">
-            <div class="container">
-                <div class="row"/>
+            <div id='cadastro' className='container'>
+                <div class="row">
                     <div class="col-md">
                         <div class="box-prod">
-                            <a class="box-prod" href="">Foto</a>
+                            <input type="file" />
                         </div>
                     </div>
-                    <div class="col-md infoPrd">
+
+                    <div class="col-md">
                         <label for="" class="form-label fs-4 mt-3">Nome Completo:</label>
-                        <input name="" type="text" class="form-control rounded-4 mb-5 border border-black"/>
+                        <input value={nome_produto} onChange={e => setNome_produto(e.target.value)} name="" type="text" class="form-control rounded-4 mb-5 border border-black" />
                         <label for="" class="form-label fs-4 mt-3">Tipo:</label>
-                        <input name="" type="text" class="form-control rounded-4 mb-5 border border-black"/>
+                        <input value={nome_produto} onChange={e => setNome_produto(e.target.value)} name="" type="text" class="form-control rounded-4 mb-5 border border-black" />
                         <label for="" class="form-label fs-4 mt-3">Preço:</label>
-                        <input name="" type="text" class="form-control rounded-4 mb-5 border border-black"/>
+                        <input value={nome_produto} onChange={e => setNome_produto(e.target.value)} name="" type="text" class="form-control rounded-4 mb-5 border border-black" />
                     </div>
-                    <div class="col-md infoPrd">
+                    <div class="col-md">
                         <label for="" class="form-label fs-4 mt-3">Marca:</label>
-                        <input name="" type="text" class="form-control rounded-4 border mb-5 border-black"/>
+                        <input value={nome_produto} onChange={e => setNome_produto(e.target.value)} name="" type="text" class="form-control rounded-4 border mb-5 border-black" />
                         <label for="" class="form-label fs-4 mt-3">Peso(Kg):</label>
-                        <input name="" type="text" class="form-control rounded-4 border mb-5 border-black"/>
+                        <input value={nome_produto} onChange={e => setNome_produto(e.target.value)} name="" type="text" class="form-control rounded-4 border mb-5 border-black" />
                         <label for="" class="form-label fs-4 mt-3">Categoria:</label>
                         <select class="form-select form-select-md rounded-4 border border-black mb-5" aria-label="Medium select example">
                             <option selected>Selecione uma categoria:</option>
@@ -32,9 +72,9 @@ import React from 'react'
                         </select>
                     </div>
                 </div>
-                <button type="submit" class="btn border border-black rounded-4 fs-4">Cadastrar-se</button>
-            <div/>
+            </div>
+            <button type="submit" class="btn border border-black rounded-4 fs-4 float-end">Cadastrar</button>
         </div>
-  )
+    )
 }
-export default cadastroProdMer
+export default CadastroProdMer
